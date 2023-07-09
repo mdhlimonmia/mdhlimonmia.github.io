3edef8f663824dd31ce52a516efa16d286265a4d
Mdhlimon <mdhlimonmia@gmail.com>
2023-07-09 12:15:43 +0600

    contact section done

--- script.js
+++ script.js
@@ -55,3 +55,23 @@ circle.forEach(point => {
         pointsMarked[i].classList.add('marked');
     }
 })
+
+//active menu ///////////////////////
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+//sticky navbar //////////////////
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", this.window.scrollY > 50)
+})
