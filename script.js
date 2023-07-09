e66deb95c6a94d62d712b230358f8a62ec391e64
Mdhlimon <mdhlimonmia@gmail.com>
2023-07-09 14:56:36 +0600

    responsive done

--- script.js
+++ script.js
@@ -75,3 +75,20 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 50)
 })
+
+
+//toggle icon navbar///////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+
+window.onscroll = () =>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+
